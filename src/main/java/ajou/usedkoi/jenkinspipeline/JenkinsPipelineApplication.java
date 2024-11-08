78dff672454ea8b00f4a8ff0ea2e1124d8f0194f
usedkoi <usedkoi@ajou.ac.kr>
2024-11-08 09:28:57 +0900

    initialize

---
+++ src/main/java/ajou/usedkoi/jenkinspipeline/JenkinsPipelineApplication.java
@@ -0,0 +1,13 @@
+package ajou.usedkoi.jenkinspipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsPipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ajou/usedkoi/jenkinspipeline/JenkinsPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package ajou.usedkoi.jenkinspipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

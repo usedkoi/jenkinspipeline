bbde03e9a69eb6487517639ccd9e6d3d3bba64a7
usedkoi <usedkoi@ajou.ac.kr>
2024-11-08 10:11:14 +0900

    calculator

---
+++ src/main/java/ajou/usedkoi/jenkinspipeline/CalculatorController.java
@@ -0,0 +1,18 @@
+package ajou.usedkoi.jenkinspipeline;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class CalculatorController {
+    private final Calculator calculator;
+
+    @RequestMapping("/sum")
+    String sum(@RequestParam("a") Integer a,
+               @RequestParam("b") Integer b){
+        return String.valueOf(calculator.sum(a, b));
+    }
+}
